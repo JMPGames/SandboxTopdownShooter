28e5feacff33f16a22d8fb1866b55c24eb2776f7
Matt Painter <79821595+JMPGames@users.noreply.github.com>
2021-10-18 21:51:49 -0400

    EnemyController refactor

--- SandBoxTDS/Assets/_Scripts/Entities/Enemy/EnemyController.cs
+++ SandBoxTDS/Assets/_Scripts/Entities/Enemy/EnemyController.cs
@@ -1,38 +1,33 @@
 ﻿using UnityEngine;
 
-/* 
-    This class handles enemy movement and attacking logic.
-    Random movement, chasing/running away from the player
-    Checking if the player is in range to attack, and attacking.
- */
-
 public enum EnemyType { RANGED, MELEE, PATTERNED }
 
 [RequireComponent(typeof(MobileEntity))]
+[RequireComponent(typeof(EnemySightHandler))]
 public class EnemyController : Entity, IMobile {
-    const float LoseSightMod = 1.5f;
     const float MinPatrolTime = 3.0f;
     const float MaxPatrolTime = 6.0f;
 
-    [SerializeField] float sightRange;
+    EnemySightHandler sh;
+
+    Vector3 patrolDirection;
+    float patrolTimer;
+    bool Patrolling { get { return patrolTimer > 0.0f; } }
+
+    float attackTimer;
+    bool AttackTimerZero { get { return attackTimer <= 0.0f; } }
+
+    #region Inspector Variables
+    [SerializeField] EnemyType enemyType;
     [SerializeField] int damage;
     [SerializeField] float attackRange;
     [SerializeField] float attackSpeed;
     [SerializeField] float attackToMoveTime;
-
-    Transform target;
-    float distanceToTarget;
-    bool targetSpotted;
-    float attackTimer;
-    Vector3 patrolDirection;
-    float patrolTimer;
-
-    bool TargetInAttackRange { get { return distanceToTarget <= attackRange; } }
-    bool AttackTimerZero { get { return attackTimer <= 0.0f; } }
-    bool Patrolling { get { return patrolTimer > 0.0f; } }
+    #endregion
 
     void Start() {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        sh = GetComponent<EnemySightHandler>();
+        sh.Setup(enemyType);        
     }
 
     void Update() {
@@ -42,9 +37,9 @@ public class EnemyController : Entity, IMobile {
 
     //Called every frame
     public (Vector3, int) GetMove() {
-        CheckForTargetInSight();
-        if (targetSpotted) {
-            GetTargetSpottedFacing();
+        sh.CheckForTargetInSight();
+        if (sh.TargetSpotted) {
+            sh.GetTargetSpottedFacing(EntityType == EntityType.CRITTER);
 
             if (CanAttack()) {
                 Attack();
@@ -52,30 +47,19 @@ public class EnemyController : Entity, IMobile {
             }
             return (Vector3.forward, Slowed ? 0 : 1);
         }
-        GetRandomFacing();
+        transform.LookAt(GetRandomFacing());
         return (Vector3.forward, 0);
     }
 
-    void CheckForTargetInSight() {
-        distanceToTarget = Vector3.Distance(transform.position, target.position);
-
-        if (targetSpotted && distanceToTarget > (sightRange * LoseSightMod)) {
-            targetSpotted = false;
-        }
-        else if (distanceToTarget < sightRange) {
-            targetSpotted = true;
-        }
-    }
-
     bool CanAttack() {
-        return TargetInAttackRange && AttackTimerZero && CanAct;
+        return !sh.TargetTooClose() && TargetInAttackRange() && AttackTimerZero && CanAct;
     }
 
-    bool InbetweenAttacks() {
-        return !AttackTimerZero && CanAct && !TargetInAttackRange;
+    bool TargetInAttackRange() {
+        return sh.DistanceToTarget() <= attackRange;
     }
 
-    void Attack() {
+    public virtual void Attack() {
         //animation, fire bullet/melee hit check, sound
         EntityState = EntityState.SLOWED_NOACT;
         NextState = EntityState.NOACT;
@@ -83,26 +67,17 @@ public class EnemyController : Entity, IMobile {
         StateTimer = attackToMoveTime;
     }
 
-    void GetTargetSpottedFacing() {
-        Vector3 direction = target.position - transform.position;
-        if (EntityType == EntityType.CRITTER) {
-            transform.LookAt(new Vector3(-direction.x, transform.position.y, -direction.z));
-        }
-        else {
-            transform.LookAt(new Vector3(direction.x, transform.position.y, direction.z));
-        }
-    }
-
     Vector3 GetRandomFacing() {
         if (Patrolling) {
             return patrolDirection;
         }
         patrolDirection = new Vector3(0.0f, Random.Range(-180f, 180f), 0.0f);
+        patrolTimer = Random.Range(MinPatrolTime, MaxPatrolTime);
         return patrolDirection;
-    }
+    }    
 
     void UpdatePatrolTimer() {
-        if (!targetSpotted && Patrolling) {
+        if (!sh.TargetSpotted && Patrolling) {
             patrolTimer -= Time.deltaTime;
         }
     }
---
+++ SandBoxTDS/Assets/_Scripts/Entities/Enemy/EnemySightHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EnemySightHandler : MonoBehaviour {
+    const float LoseSightMod = 1.5f;
+
+    Transform target;
+    EnemyType enemyType;
+    public bool TargetSpotted { get; private set; }
+
+    [SerializeField] float sightRange;
+    [SerializeField] float minDistanceFromTarget;
+
+    public void Setup(EnemyType enemyType) {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        this.enemyType = enemyType;
+    }
+
+    public void CheckForTargetInSight() {
+        if (TargetSpotted && DistanceToTarget() > (sightRange * LoseSightMod)) {
+            TargetSpotted = false;
+        }
+        else if (!TargetSpotted && DistanceToTarget() < sightRange) {
+            TargetSpotted = true;
+        }
+    }
+
+    public void GetTargetSpottedFacing(bool critter) {
+        Vector3 direction = target.position - transform.position;
+        if (TargetTooClose() || critter) {
+            transform.LookAt(new Vector3(-direction.x, transform.position.y, -direction.z));
+        }
+        else {
+            transform.LookAt(new Vector3(direction.x, transform.position.y, direction.z));
+        }
+    }
+
+    public float DistanceToTarget() {
+        return Vector3.Distance(transform.position, target.position);
+    }
+
+    public bool TargetTooClose() {
+        return enemyType == EnemyType.RANGED && DistanceToTarget() <= minDistanceFromTarget;
+    }
+}
